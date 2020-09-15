ee89f40418fbb3c09e3d33edc57b10cbe86ed260
Ben Reed <benreed@google.com>
2020-09-14 17:34:25 -0700

    Move controller constants to config package
    This commit moves most constants that are declared in the controller
    into the config package. They are alphabetically sorted in a new
    constants.go file. This change allows the constants to be used across
    packages.

--- config/constants.go
+++ config/constants.go
@@ -17,26 +17,78 @@ limitations under the License.
 package config
 
 const (
-	// LoadTestLabel is a label which contains the test's unique name.
-	LoadTestLabel = "loadtest"
-
-	// RoleLabel is a label with the role  of a test component. For
-	// example, "loadtest-role=server" indicates a server component.
-	RoleLabel = "loadtest-role"
-
-	// ComponentNameLabel is a label used to distinguish between test
-	// components with the same role.
-	ComponentNameLabel = "loadtest-component"
-
-	// ServerRole is the value the controller expects for the RoleLabel
-	// on a server component.
-	ServerRole = "server"
+	// BuildInitContainerName holds the name of the init container that assembles
+	// a binary or other bundle required to run the tests.
+	BuildInitContainerName = "build"
 
 	// ClientRole is the value the controller expects for the RoleLabel
 	// on a client component.
 	ClientRole = "client"
 
+	// CloneGitRefEnv specifies the name of the env variable that contains the
+	// commit, tag or branch to checkout after cloning a git repository.
+	CloneGitRefEnv = "CLONE_GIT_REF"
+
+	// CloneInitContainerName holds the name of the init container that obtains
+	// a copy of the code at a specific point in time.
+	CloneInitContainerName = "clone"
+
+	// CloneRepoEnv specifies the name of the env variable that contains the git
+	// repository to clone.
+	CloneRepoEnv = "CLONE_REPO"
+
+	// ComponentNameLabel is a label used to distinguish between test
+	// components with the same role.
+	ComponentNameLabel = "loadtest-component"
+
 	// DriverRole is the value the controller expects for the RoleLabel
 	// on a driver component.
 	DriverRole = "driver"
+
+	// LoadTestLabel is a label which contains the test's unique name.
+	LoadTestLabel = "loadtest"
+
+	// ReadyInitContainerName holds the name of the init container that blocks a
+	// driver from running until all worker pods are ready.
+	ReadyInitContainerName = "ready"
+
+	// ReadyMountPath is the absolute path where the ready volume should be
+	// mounted in both the ready init container and the driver's run container.
+	ReadyMountPath = "/var/data/qps_workers"
+
+	// ReadyOutputFile is the name of the file where the ready init container
+	// should write all IP addresses and port numbers for ready workers.
+	ReadyOutputFile = ReadyMountPath + "/addresses"
+
+	// ReadyVolumeName is the name of the volume that permits sharing files
+	// between the ready init container and the driver's run container.
+	ReadyVolumeName = "worker-addresses"
+
+	// RoleLabel is a label with the role  of a test component. For
+	// example, "loadtest-role=server" indicates a server component.
+	RoleLabel = "loadtest-role"
+
+	// RunContainerName holds the name of the main container where the test is
+	// executed.
+	RunContainerName = "run"
+
+	// ScenariosFileEnv specifies the name of an env variable that specifies the
+	// path to a JSON file with scenarios.
+	ScenariosFileEnv = "SCENARIOS_FILE"
+
+	// ScenariosMountPath specifies where the JSON file with the scenario should
+	// be mounted in the driver container.
+	ScenariosMountPath = "/src/scenarios"
+
+	// ServerRole is the value the controller expects for the RoleLabel
+	// on a server component.
+	ServerRole = "server"
+
+	// WorkspaceMountPath contains the path to mount the volume identified by
+	// `workspaceVolume`.
+	WorkspaceMountPath = "/src/workspace"
+
+	// WorkspaceVolumeName contains the name of the volume that is shared between
+	// the init containers and containers for a driver or worker pod.
+	WorkspaceVolumeName = "workspace"
 )
