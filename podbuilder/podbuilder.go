4100cfcd1203ae7876b6b251e20d7ca1bca3ec7f
Donghao Qiu <donghaoqiu@google.com>
2021-04-14 23:59:55 +0000

    Remove the build TIMEOUT argument for ready container; Set the environment variable while running. now the ready container's timeout would be equal to the loadtest timeout

--- podbuilder/podbuilder.go
+++ podbuilder/podbuilder.go
@@ -52,7 +52,6 @@ func addReadyInitContainer(defs *config.Defaults, test *grpcv1.LoadTest, podspec
 		Name:  "QPS_WORKERS_FILE",
 		Value: config.ReadyOutputFile,
 	})
-
 	container.VolumeMounts = append(container.VolumeMounts, corev1.VolumeMount{
 		Name:      config.ReadyVolumeName,
 		MountPath: config.ReadyMountPath,
@@ -95,6 +94,10 @@ func newReadyContainer(defs *config.Defaults, test *grpcv1.LoadTest) corev1.Cont
 			{
 				Name:  "READY_OUTPUT_FILE",
 				Value: config.ReadyOutputFile,
+			},
+			{
+				Name:  "READY_TIMEOUT",
+				Value: fmt.Sprintf("%d%s", test.Spec.TimeoutSeconds, "s"),
 			},
 		},
 		VolumeMounts: []corev1.VolumeMount{
