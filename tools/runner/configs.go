c53765e719181aeeeeee457c1d67fe2579427186
Paulo Castello da Costa <pcastello@google.com>
2021-05-03 09:01:04 -0700

    Test runner tool.

---
+++ tools/runner/configs.go
@@ -0,0 +1,82 @@
+/*
+Copyright 2021 gRPC authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package runner contains code for a test runner that can run a list of
+// load tests, wait for them to complete, and report on the results.
+package runner
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"sigs.k8s.io/yaml"
+
+	grpcv1 "github.com/grpc/test-infra/api/v1"
+)
+
+// DecodeFromFiles reads LoadTest configurations from a set of files.
+// Each file is a multipart YAML file containing LoadTest configurations.
+func DecodeFromFiles(fileNames []string) ([]*grpcv1.LoadTest, error) {
+	var configs []*grpcv1.LoadTest
+	for _, fileName := range fileNames {
+		c, err := decodeFromFile(fileName)
+		if err != nil {
+			return nil, err
+		}
+		configs = append(configs, c...)
+	}
+	return configs, nil
+}
+
+// decodeFromFile reads LoadTest configurations from a single file.
+func decodeFromFile(fileName string) ([]*grpcv1.LoadTest, error) {
+	var configs []*grpcv1.LoadTest
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	scanner := bufio.NewScanner(f)
+	for {
+
+		config, err := decodeNext(scanner)
+		if err != nil {
+			return nil, fmt.Errorf("error decoding config from %q: %v", fileName, err)
+		}
+		if config == nil {
+			break
+		}
+		configs = append(configs, config)
+	}
+	return configs, nil
+}
+
+// decodeNext decodes the next LoadTest configuration found in the file.
+func decodeNext(scanner *bufio.Scanner) (*grpcv1.LoadTest, error) {
+	const sep = "---"
+	var lines []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == sep {
+			break
+		}
+		lines = append(lines, line)
+	}
+	if len(lines) == 0 {
+		return nil, nil
+	}
+	config := new(grpcv1.LoadTest)
+	err := yaml.Unmarshal([]byte(strings.Join(lines, "\n")), config)
+	return config, err
+}
---
+++ tools/runner/runner.go
@@ -0,0 +1,106 @@
+/*
+Copyright 2021 gRPC authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package runner contains code for a test runner that can run a list of
+// load tests, wait for them to complete, and report on the results.
+package runner
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	grpcv1 "github.com/grpc/test-infra/api/v1"
+	clientset "github.com/grpc/test-infra/clientset"
+)
+
+// AfterIntervalFunction returns a function that stops for a time interval.
+// This function is provided so it can be replaced with a fake for testing.
+func AfterIntervalFunction(d time.Duration) func() {
+	return func() {
+		<-time.After(d)
+	}
+}
+
+// Runner contains the information needed to run multiple sets of LoadTests.
+type Runner struct {
+	Done           chan string
+	loadTestGetter clientset.LoadTestGetter
+	afterInterval  func()
+}
+
+// NewRunner creates a new Runner object.
+func NewRunner(loadTestGetter clientset.LoadTestGetter, afterInterval func()) *Runner {
+	return &Runner{
+		Done:           make(chan string),
+		loadTestGetter: loadTestGetter,
+		afterInterval:  afterInterval,
+	}
+}
+
+// Run runs a set of LoadTests at a given concurrency level.
+func (r *Runner) Run(qName string, configs []*grpcv1.LoadTest, concurrencyLevel int) {
+	done := make(chan int)
+	n := 0
+	for i, config := range configs {
+		if n < concurrencyLevel {
+			go r.runTest(qName, config, i, done)
+			n++
+			continue
+		}
+		<-done
+		n--
+	}
+	for n > 0 {
+		<-done
+		n--
+	}
+	r.Done <- qName
+}
+
+// runTest creates a single LoadTest and monitors it to completion.
+func (r *Runner) runTest(qName string, config *grpcv1.LoadTest, i int, done chan int) {
+	id := fmt.Sprintf("%-14s %3d", qName, i)
+	loadTest, err := r.loadTestGetter.Create(config, metav1.CreateOptions{})
+	if err != nil {
+		log.Printf("[%s] Failed to create test %s", id, config.Name)
+		done <- i
+		return
+	}
+	log.Printf("[%s] Created test %s", id, config.Name)
+	config.Status = loadTest.Status
+	for {
+		loadTest, err := r.loadTestGetter.Get(config.Name, metav1.GetOptions{})
+		if err != nil {
+			log.Printf("[%s] Failed to poll test %s", id, config.Name)
+			done <- i
+			return
+		}
+		config.Status = loadTest.Status
+		if loadTest.Status.State.IsTerminated() {
+			log.Printf("[%s] %s", id, loadTest.Status.State)
+			done <- i
+			return
+		}
+		if loadTest.Status.State == grpcv1.Running {
+			log.Printf("[%s] %s", id, loadTest.Status.State)
+			r.afterInterval()
+			continue
+		}
+		// Use a longer polling interval for tests that have not started.
+		r.afterInterval()
+		r.afterInterval()
+	}
+}
