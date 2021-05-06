535f97e0765a2eef92ca1b928f4748c2ab591a60
Jan Tattermusch <jtattermusch@google.com>
2021-05-06 12:03:09 +0200

    ruby pre-built: building ruby codegen plugins does not seem required for ruby qps_worker

--- containers/pre_built_workers/ruby/build_qps_worker.sh
+++ containers/pre_built_workers/ruby/build_qps_worker.sh
@@ -18,14 +18,3 @@ set -ex
 bundle install
 
 rake compile
-
-# build grpc_ruby_plugin
-mkdir -p cmake/build
-pushd cmake/build
-cmake -DgRPC_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release ../..
-make protoc grpc_ruby_plugin -j8
-popd
-
-# unbreak subsequent make builds by restoring zconf.h (previously renamed by cmake build)
-# see https://github.com/madler/zlib/issues/133
-(cd third_party/zlib; git checkout zconf.h)
