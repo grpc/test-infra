8da20af42003520d320c5c98c0444cae90c638c4
Wanlin Du <wanlindu@google.com>
2021-11-10 12:22:34 -0800

    Add a grpc server to update the endpoint

--- tools/xds/api/v1/config.go
+++
@@ -1,8 +0,0 @@
-package v1
-
-// Endpoint struct defines the address and port of
-// benchmark test server.
-type Endpoint struct {
-	Address string `yaml:"address"`
-	Port    uint   `yaml:"port"`
-}
