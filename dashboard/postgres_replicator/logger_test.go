c76deeea2fd0fad8d1825116e474da7402bc08c9
Brandon Paiz <brandonpaiz@google.com>
2021-12-23 22:11:52 +0000

    Fix testing

--- dashboard/postgres_replicator/logger_test.go
+++ dashboard/postgres_replicator/logger_test.go
@@ -2,35 +2,33 @@ package transfer
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestNewLogger(t *testing.T) {
-	tableName := "foobar"
-	logger := NewLogger(tableName)
-	expectedPrefix := fmt.Sprintf("[%s] ", tableName)
-	actualPrefix := logger.Prefix()
-	if actualPrefix != expectedPrefix {
-		t.Errorf(`Expected prefix "%s". Actual prefix: "%s"`, tableName, actualPrefix)
+	logger := NewLogger("testTable")
+	want := "[testTable] "
+	got := logger.Prefix()
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf(`Logger.NewLogger("testTable") diff (-want +got):\n%s`, diff)
 	}
 }
 
 func TestErrorf(t *testing.T) {
 	// Test that "Error: " is prefixed to the format string.
 	var buf bytes.Buffer
-	tableName := "foobar"
-	errorMsg := "Sample error"
-	expectedLog := fmt.Sprintf("[%s] Error: %s\n", tableName, errorMsg)
-	logger := NewLogger(tableName)
+	logger := NewLogger("testTable")
+	want := "[testTable] Error: Sample error\n"
 
 	logger.SetOutput(&buf)
 	logger.SetFlags(log.Lmsgprefix)
-	logger.Errorf(errorMsg)
-	logString := buf.String()
+	logger.Errorf("Sample error")
+	got := buf.String()
 
-	if logString != expectedLog {
-		t.Errorf(`Expected log "%s". Actual log "%s"`, expectedLog, logString)
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Logger.Errorf() diff (-want +got):\n%s", diff)
 	}
 }
