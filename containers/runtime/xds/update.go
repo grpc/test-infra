9279dc887167a6b8b9d7f09e73d0d2676f8c23ee
Wanlin Du <wanlindu@google.com>
2022-01-07 14:12:44 -0800

    update the endpoints

--- containers/runtime/xds/update.go
+++ containers/runtime/xds/update.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 
+	config "github.com/grpc/test-infra/containers/runtime/xds/config"
 	pb "github.com/grpc/test-infra/proto/endpointupdater"
 	grpc "google.golang.org/grpc"
 )
@@ -18,26 +19,28 @@ var srv *grpc.Server
 // UpdateServer is used to implement endpointupdater.EndpointUpdater.
 type UpdateServer struct {
 	pb.UnimplementedEndpointUpdaterServer
-	EndpointAddress chan string
-	EndpointPort    chan uint32
+	EndpointsChannel chan []*config.TestEndpoint
 }
 
 // UpdateEndpoint implements endpointupdater.EndpointUpdater
 func (us *UpdateServer) UpdateEndpoint(ctx context.Context, in *pb.EndpointUpdaterRequest) (*pb.EndpointUpdaterReply, error) {
-	us.EndpointAddress <- in.GetIpAddress()
-	us.EndpointPort <- in.GetPort()
-	log.Printf("Received endpoint: %v:%v", us.EndpointAddress, us.EndpointPort)
+	var testEnpoints []*config.TestEndpoint
+	for _, c := range in.GetEndpoints() {
+		testEnpoints = append(testEnpoints, &config.TestEndpoint{TestUpstreamHost: c.IpAddress, TestUpstreamPort: c.Port})
+		log.Printf("Received endpoint: %v:%v", c.IpAddress, c.Port)
+	}
+	us.EndpointsChannel <- testEnpoints
 	return &pb.EndpointUpdaterReply{}, nil
 }
 
 // RunUpdateServer start a gRPC server listening to test server address and port
-func RunUpdateServer(testHostAddress chan string, testPort chan uint32) {
+func RunUpdateServer(endpointChannel chan []*config.TestEndpoint) {
 	lis, err := net.Listen("tcp", updatePort)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	srv = grpc.NewServer()
-	pb.RegisterEndpointUpdaterServer(srv, &UpdateServer{EndpointAddress: testHostAddress, EndpointPort: testPort})
+	pb.RegisterEndpointUpdaterServer(srv, &UpdateServer{EndpointsChannel: endpointChannel})
 	log.Printf("Endpoint update server listening at %v", lis.Addr())
 	if err := srv.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
