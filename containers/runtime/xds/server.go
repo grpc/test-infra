308da31a1319c5c5b10b640aa70b21140e6f9345
Wanlin Du <wanlindu@google.com>
2022-01-07 12:59:59 -0800

    Clean up

--- containers/runtime/xds/server.go
+++ containers/runtime/xds/server.go
@@ -13,6 +13,9 @@ import (
 	endpointservice "github.com/envoyproxy/go-control-plane/envoy/service/endpoint/v3"
 	listenerservice "github.com/envoyproxy/go-control-plane/envoy/service/listener/v3"
 	routeservice "github.com/envoyproxy/go-control-plane/envoy/service/route/v3"
+	secretservice "github.com/envoyproxy/go-control-plane/envoy/service/secret/v3"
+	runtimeservice "github.com/envoyproxy/go-control-plane/envoy/service/runtime/v3"
+	extensionconfigservice "github.com/envoyproxy/go-control-plane/envoy/service/extension/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/server/v3"
 )
 
@@ -22,7 +25,12 @@ func registerServer(grpcServer *grpc.Server, server server.Server) {
 	endpointservice.RegisterEndpointDiscoveryServiceServer(grpcServer, server)
 	clusterservice.RegisterClusterDiscoveryServiceServer(grpcServer, server)
 	routeservice.RegisterRouteDiscoveryServiceServer(grpcServer, server)
+	routeservice.RegisterScopedRoutesDiscoveryServiceServer(grpcServer, server)
 	listenerservice.RegisterListenerDiscoveryServiceServer(grpcServer, server)
+	secretservice.RegisterSecretDiscoveryServiceServer(grpcServer, server)
+	runtimeservice.RegisterRuntimeDiscoveryServiceServer(grpcServer, server)
+	extensionconfigservice.RegisterExtensionConfigDiscoveryServiceServer(grpcServer, server)
+
 }
 
 // RunxDSServer starts an xDS server at the given port.
@@ -41,3 +49,5 @@ func RunxDSServer(ctx context.Context, srv server.Server, port uint) {
 		log.Println(err)
 	}
 }
+
+// how to stop a server 
