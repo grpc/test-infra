1f36d527bd168db1101e7f4b6d2f8486c769be08
Paulo Castello da Costa <pcastello@google.com>
2022-01-27 21:59:42 -0800

    Update base docker images to golang:1.16.

--- podbuilder/suite_test.go
+++ podbuilder/suite_test.go
@@ -96,7 +96,7 @@ func newDefaults() *config.Defaults {
 			},
 			{
 				Language:   "go",
-				BuildImage: "golang:1.14",
+				BuildImage: "golang:1.16",
 				RunImage:   "gcr.io/grpc-fake-project/test-infra/go",
 			},
 			{
