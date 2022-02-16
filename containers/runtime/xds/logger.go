167c7407d2ebf661dd6ab6634f253ce94027f8ca
Wanlin Du <wanlindu@google.com>
2022-02-16 09:16:09 -0800

    Add copy rights and fix typos

--- containers/runtime/xds/logger.go
+++ containers/runtime/xds/logger.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2022 gRPC authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package xds
 
 import (
@@ -8,22 +21,22 @@ import (
 type Logger struct {
 }
 
-// Debugf print out debug information.
+// Debugf prints out debug information.
 func (logger Logger) Debugf(format string, args ...interface{}) {
 	log.Printf(format+"\n", args...)
 }
 
-// Infof print out useful information.
+// Infof prints out useful information.
 func (logger Logger) Infof(format string, args ...interface{}) {
 	log.Printf(format+"\n", args...)
 }
 
-// Warnf print out warnings.
+// Warnf prints out warnings.
 func (logger Logger) Warnf(format string, args ...interface{}) {
 	log.Printf(format+"\n", args...)
 }
 
-// Errorf print out the error message and stop the process.
+// Errorf prints out the error message and stops the process.
 func (logger Logger) Errorf(format string, args ...interface{}) {
 	log.Fatalf(format+"\n", args...)
 }
