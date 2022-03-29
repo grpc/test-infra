84841741383ce9ec622fa71a71cb078c120f444a
Wanlin Du <wanlindu@google.com>
2022-03-29 11:52:50 -0700

    Add prometheus query to driver's run container.
    This commit asks the driver to run prometheus queries to obtain
    cpu and memory data when running a PSM test. Currently a PSM test
    is observed when SERVER_TARGET_OVERRIDE environment variable is
    not empty.

--- containers/runtime/driver/run.sh
+++ containers/runtime/driver/run.sh
@@ -42,5 +42,11 @@ if [ -n "${BQ_RESULT_TABLE}" ]; then
         --container_name=sidecar
     fi
   fi
+  if [ -n "${SERVER_TARGET_OVERRIDE}" ]; then
+    python3 /src/code/tools/run_tests/performance/prometheus.py \
+      --url=http://prometheus.prometheus.svc.cluster.local:9090 \
+      --pod_type=driver --pod_type=clients --container_name=main \
+      --container_name=sidecar
+  fi
   python3 /src/code/tools/run_tests/performance/bq_upload_result.py --bq_result_table="${BQ_RESULT_TABLE}"
 fi
