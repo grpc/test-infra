9e4187b79c0c85e77d4554b4495193e118c2abe7
Paulo Castello da Costa <pcastello@google.com>
2022-11-21 11:49:19 -0800

    Update golang version and go mod tidy.

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -137,7 +137,7 @@ func newDefaults() *config.Defaults {
 			},
 			{
 				Language:   "go",
-				BuildImage: "golang:1.16",
+				BuildImage: "golang:1.17",
 				RunImage:   "gcr.io/grpc-fake-project/test-infra/go",
 			},
 			{
